15a09cf4f7790cc72786163d8f2c863fd09eaa52
sadwd479-max <sadwd479@gmail.com>
2026-07-30 00:39:00 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Конфигурация Vite с правильным базовым путем для GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-  base: '/dota-analizator-beta/',
-})
